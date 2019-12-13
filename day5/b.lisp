35a81fbefdbf73a91f7ca9ea1a2b305806babfdb
Tjeerd in 't Veen <tjeerd@twinapps.co>
2019-12-13 08:53:08 +0100

    cleaning up

--- day5/b.lisp
+++ day5/b.lisp
@@ -8,82 +8,70 @@
   (setf *instructions* (make-array (length list) :initial-contents list))
   (compute 0 5)) ;; passing input 5
 
-(defun compute (position &optional input)
+(defun compute (pos &optional input)
   (multiple-value-bind (opcode first-mode second-mode)
-      (extract-instruction (elt *instructions* position))
-  (case opcode
-    (1
-     ;; TODO Remove new-instructions, just call setf directly
-     (let ((new-instructions (set-value (+
-                                         (value-at 1)
-                                         (value-at 2))
-                                        (+ 3 position))))
-       ;; TODO not needed
-       (setf *instructions* new-instructions)
-       (compute (+ 4 position))))
-    (2
-     (let ((new-instructions (set-value (*
-                                         (value-at 1)
-                                         (value-at 2))
-                                        (+ 3 position))))
+      (extract-instruction (elt *instructions* pos))
+    (case opcode
+      (1
+       (set-value (+ (get-value 1)
+                     (get-value 2))
+                  (+ 3 pos))
+       (compute (+ 4 pos)))
+      (2
+       (set-value (* (get-value 1)
+                     (get-value 2))
+                  (+ 3 pos))
+       (compute (+ 4 pos)))
+      (3
+       (set-value input (+ pos 1))
+       (compute (+ 2 pos)))
+      (4
+       (format t "Output is: ~a~%" (get-value 1))
+       (compute (+ 2 pos)))
+      (5
+       (if (not (eq 0 (get-value 1)))
+           (compute (get-value 2))
+           (compute (+ 3 pos))))
+      (6
+       (if (eq 0 (get-value 1))
+           (compute (get-value 2))
+           (compute (+ 3 pos))))
+      (7
+       (if (< (get-value 1)
+              (get-value 2))
+           (progn (set-value 1 (+ 3 pos))
+                  (compute (+ 4 pos)))
+           (progn (set-value 0 (+ 3 pos))
+                  (compute (+ 4 pos)))))
+      (8
+       (if (eql (get-value 1)
+                (get-value 2))
+           (progn
+             (set-value 1 (+ 3 pos))
+             (compute (+ 4 pos)))
+           (progn
+             (set-value 0 (+ 3 pos))
+             (compute (+ 4 pos)))))
+      (99 ;; halt
+       (format t "I'm finished"))
+      (t ;; Unknown opcode
+       (format t "Error, opcode ~a received~%" opcode)
+       ))))
 
-       (setf *instructions* new-instructions)
-       (compute (+ 4 position))))
-    (3
-     (set-value input (+ position 1))
-     (compute (+ 2 position)))
-    (4
-     (format t "Output is: ~a~%" (value-at 1))
-     (compute (+ 2 position)))
-    (5
-     (if (not (eq 0 (value-at 1)))
-         (compute
-                  (value-at 2))
-         (compute (+ 3 position))))
-    (6
-     (if (eq 0 (value-at 1))
-           (compute
-                    (value-at 2))
-           (compute (+ 3 position))))
-    (7
-     ;; (print "opcode 7")
-     (if (< (value-at 1)
-            (value-at 2))
-         (progn (set-value 1 (+ 3 position))
-                (compute (+ 4 position)))
-         (progn (set-value 0 (+ 3 position))
-                (compute (+ 4 position)))))
-    (8
-     (if (eql (value-at 1)
-            (value-at 2))
-         (progn
-           (set-value 1 (+ 3 position))
-           (compute (+ 4 position)))
-         (progn
-           (set-value 0 (+ 3 position))
-           (compute (+ 4 position)))))
-
-    ((eq opcode 99) ;; Halt
-     (format t "I'm finished"))
-    (t
-     (format t "Error, opcode ~a received~%" opcode)
-     ))))
-
-(defmacro value-at (pos)
+(defmacro get-value (pos)
   (if (eql 1 pos)
-      `(get-value (+ ,pos position) first-mode)
-      `(get-value (+ ,pos position) second-mode)))
+      `(real-value (+ ,pos pos) first-mode)
+      `(real-value (+ ,pos pos) second-mode)))
 
-(defun get-value (position mode)
-  (print "a")
+(defun real-value (pos mode)
   (if (eq mode 0)
-      (elt-reference position *instructions*) ;; Position mode, get value at position
-      (elt *instructions* position)));; Immediate mode, get value directly
+      (eltref *instructions* pos) ;; Pos mode, get value at pos
+      (elt *instructions* pos)));; Immediate mode, get value directly
 
-;; Position is always resolved to position mode
-(defun set-value (value position)
-  (let ((actual-position (elt *instructions* position))) ;; A value that's stored is always in position mode (at least never in immediate mode)
-    (setf (aref *instructions* actual-position) value))
+;; Pos is always resolved to pos mode
+(defun set-value (value pos)
+  (let ((actual-pos (elt *instructions* pos))) ;; A value that's stored is always in pos mode (at least never in immediate mode)
+    (setf (aref *instructions* actual-pos) value))
   *instructions*
   )
 
@@ -95,6 +83,6 @@
         (declare (ignore _))
         (values opcode first-mode second-mode)))))
 
-(defun elt-reference (index arr)
+(defun eltref (sequence index)
   "Find the element in the array stored in the index"
-  (elt arr (elt arr index)))
+  (elt sequence (elt sequence index)))
